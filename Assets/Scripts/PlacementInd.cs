de7b01b7c45dd9e2546bf560277d738581a776ff
Johannes Kastner <johannes.kastner@yahoo.de>
2023-01-07 15:56:02 +0100

    etr

--- Assets/Scripts/PlacementInd.cs
+++ Assets/Scripts/PlacementInd.cs
@@ -11,17 +11,26 @@ public class PlacementInd : MonoBehaviour
     public GameObject objectToPlace;
     public GameObject placementIndicator;
 
+    public GameObject[] tapePoints;
+
+    int currentTapePoint = 0;
+
     private ARSessionOrigin arOrigin;
     ARRaycastManager aRRaycastManager;
+    public Button plusButton;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
+    private bool plusButtonPressed = false;
+
+    public LineRenderer line;
 
     void Start()
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
         aRRaycastManager = GetComponent<ARRaycastManager>();
+        plusButton.onClick.AddListener(OnPlusButtonClick);
 
-        
+
     }
 
     void Update()
@@ -29,15 +38,46 @@ public class PlacementInd : MonoBehaviour
         UpdatePlacementPose();
         UpdatePlacementIndicator();
 
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (plusButtonPressed == true || currentTapePoint < 2)
         {
-            PlaceObject();
+            PlacePoint(placementPose.position, currentTapePoint);
+            currentTapePoint += 1;
+
+            plusButtonPressed = false;
         }
+        DrawLine();
     }
 
-    private void PlaceObject()
+
+
+    public void PlacePoint(Vector3 pointPosition, int pointIndex)
     {
-        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+        tapePoints[pointIndex].SetActive(true);
+
+        tapePoints[pointIndex].transform.position = pointPosition;
+
+        if (currentTapePoint == 1)
+        {
+            DrawLine();
+        }
+
+        currentTapePoint += 1;
+    }
+
+    void DrawLine()
+    {
+        line.enabled = true;
+        line.SetPosition(0, placementPose.position);
+        if (currentTapePoint == 1)
+        {
+            line.SetPosition(1, placementPose.position);
+
+        }
+        else if (currentTapePoint == 2)
+        {
+            line.SetPosition(1, tapePoints[1].transform.position);
+
+        }
     }
 
     private void UpdatePlacementIndicator()
@@ -68,5 +108,9 @@ public class PlacementInd : MonoBehaviour
             var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
             placementPose.rotation = Quaternion.LookRotation(cameraBearing);
         }
+    }
+    public void OnPlusButtonClick()
+    {
+        plusButtonPressed = true;
     }
 }
